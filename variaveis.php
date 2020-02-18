d42f482b1fd9154a59a1b0ab1dc696ef1bdf64d7
felipe <felipefo@gmail.com>
2020-02-18 14:47:25 -0300

    .
    .

---
+++ variaveis.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+    <title>
+        PHP Variaveis
+    </title>
+</head>    
+<body>
+<?php
+
+$teste = 5;
+$teste = "string";
+echo $teste . "<br>";
+$teste = 5;
+echo $teste;
+
+$nome = "João Alberto";
+$nome1 = $nome;
+
+echo "<p> Nome: " . $nome . "<p>";
+echo "<p> Nome1:" . $nome1 . "<p>";
+
+$nome = "Andre";
+
+echo "<p> Nome: " . $nome . "<p>";
+echo "<p> Nome1:" . $nome1 . "<p>";
+
+
+?>
+</body>
+</html>
